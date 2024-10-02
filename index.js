1a99e3201b5adb4faffe837fcb5ed6a38a287b75
adan <adanabdullahi343@gmail.com>
2024-10-02 21:58:14 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// 1. Declare a global variable 'customerName' using 'var'
+var customerName = 'bob';
+
+// 2. Function to uppercase 'customerName'
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); // Modifies the global 'customerName'
+}
+
+// 3. Function to set 'bestCustomer' to 'not bob'
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Declares 'bestCustomer' in global scope
+}
+
+// 4. Function to overwrite 'bestCustomer'
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; // Changes the global 'bestCustomer' variable
+}
+
+// 5. Declare 'leastFavoriteCustomer' as a constant and attempt to change it
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another value'; // This will throw an error because constants cannot be reassigned
+}
